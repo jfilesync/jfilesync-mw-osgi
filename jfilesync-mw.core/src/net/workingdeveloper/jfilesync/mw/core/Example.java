6428efa75a9235a538f4c1758e1a9c547db8fbb0
Christoph Graupner <ch.graupner@workingdeveloper.net>
2018-08-19 16:21:46 +0200

    added base jfilesync-mw.core bundle project

---
+++ jfilesync-mw.core/src/net/workingdeveloper/jfilesync/mw/core/Example.java
@@ -0,0 +1,10 @@
+package net.workingdeveloper.jfilesync.mw.core;
+
+import org.osgi.service.component.annotations.*;
+
+@Component
+public class Example {
+
+	// TODO: class provided by template
+
+}
---
+++ jfilesync-mw.core/test/net/workingdeveloper/jfilesync/mw/core/ExampleTest.java
@@ -0,0 +1,14 @@
+package net.workingdeveloper.jfilesync.mw.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
