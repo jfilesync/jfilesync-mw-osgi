2087d3ca97616c48fe159e5c876030cbfa14310a
Christoph Graupner <ch.graupner@workingdeveloper.net>
2018-08-26 12:15:37 +0200

    added jfilesync-mw.core.ui.project.setup project

---
+++ jfilesync-mw.core.ui.project.setup/src/net/jfilesync/mw/core/ui/project/setup/JfsMwCoreUIProjectSetupActivator.java
@@ -0,0 +1,19 @@
+package net.jfilesync.mw.core.ui.project.setup;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class JfsMwCoreUIProjectSetupActivator implements BundleActivator {
+
+  @Override
+  public void start(BundleContext context) throws Exception {
+    // TODO Auto-generated method stub
+    System.out.println("JfsMwCoreUI Project Setup Bundle started");
+  }
+
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    // TODO Auto-generated method stub
+    System.out.println("JfsMwCoreUI Project Setup Bundle stopped");
+  }
+}
