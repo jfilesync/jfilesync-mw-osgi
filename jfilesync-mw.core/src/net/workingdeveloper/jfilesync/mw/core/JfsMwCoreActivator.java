b4d6b90424dde07c4066ccf02794196091aaa179
Christoph Graupner <ch.graupner@workingdeveloper.net>
2018-08-26 11:39:47 +0200

    adding core Activator class

---
+++ jfilesync-mw.core/src/net/workingdeveloper/jfilesync/mw/core/JfsMwCoreActivator.java
@@ -0,0 +1,19 @@
+package net.workingdeveloper.jfilesync.mw.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Christoph Graupner <ch.graupner@workingdeveloper.net>
+ */
+public class JfsMwCoreActivator implements BundleActivator {
+  @Override
+  public void start(final BundleContext context) throws Exception {
+    System.out.println("JFileSync MW Core Bundle started");
+  }
+
+  @Override
+  public void stop(final BundleContext context) throws Exception {
+    System.out.println("JFileSync MW Core Bundle stopped");
+  }
+}
