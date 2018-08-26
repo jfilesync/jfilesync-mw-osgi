5c5d8142ef661660b8d64fbcfe9c09da4736b4b7
Christoph Graupner <ch.graupner@workingdeveloper.net>
2018-08-26 12:18:15 +0200

    formatting - Bundle-ContactAddress - Bundle-DocURL

--- jfilesync-mw.core.ui.javafx.initializer/src/net/workingdevelopers/jfilesync/mw/core/ui/javafx/initializer/JfsMwCoreUiJavaFXInitializer.java
+++ jfilesync-mw.core.ui.javafx.initializer/src/net/workingdevelopers/jfilesync/mw/core/ui/javafx/initializer/JfsMwCoreUiJavaFXInitializer.java
@@ -1,32 +1,33 @@
 package net.workingdevelopers.jfilesync.mw.core.ui.javafx.initializer;
 
-import java.util.concurrent.Executors;
-
+import javafx.application.Platform;
+import net.workingdevelopers.jfilesync.mw.core.ui.javafx.initializer.provider.JfsMwCoreUiJfxApplication;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import javafx.application.Platform;
-import net.workingdevelopers.jfilesync.mw.core.ui.javafx.initializer.provider.JfsMwCoreUiJfxApplication;
+import java.util.concurrent.Executors;
 
 public class JfsMwCoreUiJavaFXInitializer implements BundleActivator {
-	public static boolean started = false;
+  private static boolean started = false;
 
-	@Override
-	public void start(BundleContext context) throws Exception {
-		if (false == started) {
-			Executors.defaultThreadFactory().newThread(() -> {
-				Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-				System.out.println("Starting JavaFX Application thread");
-				JfsMwCoreUiJfxApplication.launch(JfsMwCoreUiJfxApplication.class);
-			}).start();
-		}
+  @Override
+  public void start(BundleContext context) throws Exception {
+    if (!started) {
+      Executors.defaultThreadFactory().newThread(() -> {
+        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+        System.out.println("Starting JavaFX Application thread");
+        JfsMwCoreUiJfxApplication.launch(JfsMwCoreUiJfxApplication.class);
+      }).start();
+      started = true;
+    }
 
-		System.out.println("JavaFX initialization Bundle started");
-	}
+    System.out.println("JfsCoreUI JavaFX initialization Bundle started");
+  }
 
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		Platform.exit();
-		System.out.println("JavaFX Platform stopped");
-	}
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    Platform.exit();
+    System.out.println("JavaFX Platform stopped");
+    System.out.println("JfsCoreUI JavaFX initialization Bundle stopped");
+  }
 }
