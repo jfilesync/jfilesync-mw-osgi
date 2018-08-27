2f097e57330225d6992c2baf42217cc05eac727e
Christoph Graupner <ch.graupner@workingdeveloper.net>
2018-08-27 11:08:22 +0200

    add and set package version to 0.0.1

--- jfilesync-mw.core.ui.project.setup/src/net/jfilesync/mw/core/ui/project/setup/package-info.java
+++ jfilesync-mw.core.ui.project.setup/src/net/jfilesync/mw/core/ui/project/setup/package-info.java
@@ -5,4 +5,5 @@
  * @author Christoph Graupner <ch.graupner@workingdeveloper.net>
  *
  */
+@org.osgi.annotation.versioning.Version("0.0.1")
 package net.jfilesync.mw.core.ui.project.setup;
--- jfilesync-mw.core.ui/src/net/jfilesync/mw/core/ui/package-info.java
+++ jfilesync-mw.core.ui/src/net/jfilesync/mw/core/ui/package-info.java
@@ -1,8 +1,6 @@
 /**
- *
- */
-/**
  * @author Christoph Graupner <ch.graupner@workingdeveloper.net>
  *
  */
+@org.osgi.annotation.versioning.Version("0.0.1")
 package net.jfilesync.mw.core.ui;
